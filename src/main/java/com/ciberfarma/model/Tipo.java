327c87455b284e72a123874cc08e3e693bb4c997
unknown <SUITE@SUITECE205-ST30.cibertec.edu.pe>
2024-09-17 18:14:45 -0500

    Proyecto de Mtenimiento de Productos

---
+++ src/main/java/com/ciberfarma/model/Tipo.java
@@ -0,0 +1,22 @@
+package com.ciberfarma.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+@Entity
+@Data
+@Table(name = "tb_tipos")
+public class Tipo {
+	@Id
+	private int id_tipo;
+	private String descripcion;
+	
+	@OneToMany
+	@JoinColumn(name = "idtipo" , insertable = false, updatable = false)
+	private List<Usuario> lstUsuarios;
+}
